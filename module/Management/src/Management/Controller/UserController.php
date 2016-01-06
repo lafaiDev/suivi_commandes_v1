d69b4df55dbc63ab7dde1d2d9afe2663867aa7ad
lafaiDev <ahmed.lafai@gmail.com>
2016-01-06 11:46:15 +0000

    add new module management user

---
+++ module/Management/src/Management/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * controleur for manager user
+ */
+
+namespace Management\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class UserController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+    public function addAction()
+    {
+        return array();
+    }
+    public function editAction()
+    {
+        return array();
+    }
+    public function deleteAction()
+    {
+        return array();
+    }
+   
+}
